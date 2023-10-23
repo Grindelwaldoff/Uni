f8c4421a2bb2225e0aa8b5012211849b8d4f2089
Vseva <103783951+Grindelwaldoff@users.noreply.github.com>
2023-10-23 16:19:19 +0300

    m_pi fixed

--- First_exam/third_test.c
+++ First_exam/third_test.c
@@ -7,19 +7,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include "third_module.h"
 
 
-int calculator(double x, double p, double h)
+void calculator(void)
 {
+	double x = -1;
+    const double h = 0.2;
 	while (x <= 2)
 	{
 		if (0 < x && x <= 2) {
-            printf("Result: (%.3f, %.3f)\n", x, first_expression(x, p));
+            printf("Result: (%.3f, %.3f)\n", x, first_expression(x, M_PI));
         }
         else if (-1 <= x && x < 0) {
-            printf("Result: (%.3f, %.3f)\n", x, second_expression(x, p));
+            printf("Result: (%.3f, %.3f)\n", x, second_expression(x, M_PI));
         }
         else if (x == 0)
         {
@@ -27,17 +30,13 @@ int calculator(double x, double p, double h)
         }
         x = round((x + h) * 1000) / 1000;  // округление до 3 знаков после запятой
 	}
-	return EXIT_SUCCESS;
 }
 
 int main(void) {
-    double x = -1;
-    const double p = M_PI;
-    const double h = 0.2;
     int loop_indicator = 1;
 	printf("Vsevolod Rybnik Test 1 task 3 var. 26\n");
     while (loop_indicator) {
-        calculator(x, p, h);
+        calculator();
         printf("Wanna keep entering values? (`any num` - yep, 0-nope) ");
         scanf("%d", &loop_indicator);
     }
